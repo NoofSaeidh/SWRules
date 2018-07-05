7febdb3d8d48c4d9c9317a385551500137c0ba39
Alexey Kuznetsov <noofsaeidh@outlook.com>
2018-07-05 18:34:22 +0300

    fix entity editing

--- src/noofs.SWRules.Web/Controllers/PowersController.cs
+++ src/noofs.SWRules.Web/Controllers/PowersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.Domain.Entities;
 using Abp.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -61,19 +62,18 @@ namespace noofs.SWRules.Web.Controllers
             return View();
         }
 
-        // POST: Powers/Create
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Name,Book,Points,Duration,Distance,Rank,Trappings,Text,IsDeleted,DeleterUserId,DeletionTime,LastModificationTime,LastModifierUserId,CreationTime,CreatorUserId,Id")] Power power)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _powersRepo.Add(power);
-        //        await _powersRepo.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(power);
-        //}
+        //POST: Powers/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([PowerEditBind] Power power)
+        {
+            if (ModelState.IsValid)
+            {
+                await _powersRepo.InsertAsync(power);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(power);
+        }
 
         // GET: Powers/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -91,40 +91,37 @@ namespace noofs.SWRules.Web.Controllers
             return View(power);
         }
 
-        // POST: Powers/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("Name,Book,Points,Duration,Distance,Rank,Trappings,Text,IsDeleted,DeleterUserId,DeletionTime,LastModificationTime,LastModifierUserId,CreationTime,CreatorUserId,Id")] Power power)
-        //{
-        //    if (id != power.Id)
-        //    {
-        //        return NotFound();
-        //    }
+        //POST: Powers/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [PowerEditBind(withId: true)] Power power)
+        {
+            if (id != power.Id)
+            {
+                return NotFound();
+            }
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _powersRepo.Update(power);
-        //            await _powersRepo.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!PowerExists(power.Id))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(power);
-        //}
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _powersRepo.UpdateAsync(power);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!await PowerExists(power.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(power);
+        }
 
         // GET: Powers/Delete/5
         public async Task<IActionResult> Delete(int? id)
@@ -155,6 +152,37 @@ namespace noofs.SWRules.Web.Controllers
         private async Task<bool> PowerExists(int id)
         {
             return await _powersRepo.Query(async e => await e.AnyAsync(ee => ee.Id == id));
+        }
+
+        public class PowerEditBindAttribute : BindAttribute
+        {
+            protected static readonly string IdProperty = nameof(IEntity.Id);
+
+            protected static readonly string[] Properties =
+            {
+                nameof(Power.Book),
+                nameof(Power.Distance),
+                nameof(Power.Duration),
+                nameof(Power.Name),
+                nameof(Power.Points),
+                nameof(Power.Rank),
+                nameof(Power.Text),
+                nameof(Power.Trappings)
+            };
+
+            protected static readonly string[] PropertiesWithId;
+
+            static PowerEditBindAttribute()
+            {
+                PropertiesWithId = new string[Properties.Length + 1];
+                Array.Copy(Properties, PropertiesWithId, Properties.Length);
+                PropertiesWithId[Properties.Length] = IdProperty;
+            }
+
+            public PowerEditBindAttribute(bool withId = false) 
+                : base(withId ? PropertiesWithId : Properties)
+            {
+            }
         }
     }
 }
